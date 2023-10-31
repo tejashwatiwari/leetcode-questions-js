579038853134099050b967ae8d3fb31af2f57ea4
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2023-10-30 18:33:14 -0700

    Time: 91 ms (37.19%) | Memory: 50.6 MB (51.40%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+   nums.sort((a,b)=>a-b)
+    let minval=1;
+    for (let i=0;i<nums.length;i++){
+        while (nums[i]==minval){
+            if(nums[i]>0)
+            minval++;
+        }
+    } 
+return minval;
+
+};
