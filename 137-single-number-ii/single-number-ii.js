2101022a6cefaae48c58d88c5a33dd1aacc6d887
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2023-11-01 18:55:31 -0700

    Time: 51 ms (77.15%) | Memory: 43.3 MB (61.75%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+        let maps = {}
+    for (let i=0;i<nums.length;i++){
+        let val = maps[nums[i]];
+    if (val == undefined){
+      maps[nums[i]] = 1;
+    }else {
+      maps[nums[i]] = val +1;
+    }
+    }
+    for (key in maps){
+    if (maps[key]==1){
+        return key;
+    }
+    }
+};
