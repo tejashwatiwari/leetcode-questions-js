607cfdbaa28e329c5f040d6546dd32187d0430f5
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2023-12-31 10:07:55 -0800

    Time: 80 ms (5.10%) | Memory: 42 MB (48.21%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -4,14 +4,23 @@
  */
 var lengthOfLastWord = function(s) {
     let count = 0;
-    let r = s.length-1
-    while (s[r] == ' ') {
-        r--
+    // let r = s.length-1
+    // while (s[r] == ' ') {
+    //     r--
+    // }
+    // while (s[r]!== ' ') {
+    //     if (s[r] === undefined) break
+    //     count++ ;
+    //     r--;
+    // } 
+    // return count;
+        for(let i= s.length-1; i>=0; i--){
+        if(s[i]!=' '){
+            count++;
+            if(i>0 && s[i-1] == ' '){
+                break;
+            }
+        }
     }
-    while (s[r]!== ' ') {
-        if (s[r] === undefined) break
-        count++ ;
-        r--;
-    } 
     return count;
 };
