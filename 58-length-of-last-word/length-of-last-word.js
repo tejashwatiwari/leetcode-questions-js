4e4de05465065d834538c9749e571778ff19d54c
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2023-12-31 10:08:08 -0800

    Time: 59 ms (15.53%) | Memory: 42 MB (48.21%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -4,16 +4,6 @@
  */
 var lengthOfLastWord = function(s) {
     let count = 0;
-    // let r = s.length-1
-    // while (s[r] == ' ') {
-    //     r--
-    // }
-    // while (s[r]!== ' ') {
-    //     if (s[r] === undefined) break
-    //     count++ ;
-    //     r--;
-    // } 
-    // return count;
         for(let i= s.length-1; i>=0; i--){
         if(s[i]!=' '){
             count++;
