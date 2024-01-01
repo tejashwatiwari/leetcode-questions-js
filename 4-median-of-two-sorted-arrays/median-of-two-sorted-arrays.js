619fb5a2d90e7dac8605f6558cdb162375b4a6c0
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2023-12-31 17:10:30 -0800

    Time: 121 ms (17.21%) | Memory: 48.1 MB (42.70%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -7,8 +7,10 @@ var findMedianSortedArrays = function(nums1, nums2) {
     for (let i = 0;i<nums2.length;i++){
         nums1.push(nums2[i])
     }
-    nums1.sort((a,b)=>a-b)
-   let left=0; right=nums1.length,mid=Math.floor((left+right)/2)
-   if(nums1.length%2!==0)return nums1[mid]
-   else return (nums1[mid]+nums1[mid-1])/2
-};
+    nums1.sort((a,b)=>(a-b))
+    let l = 0, r = nums1.length, mid = Math.floor((l+r)/2)
+    if (nums1.length%2 !==0) return nums1[mid]
+    else {
+        return (nums1[mid] + nums1[mid-1])/2
+    }
+}
