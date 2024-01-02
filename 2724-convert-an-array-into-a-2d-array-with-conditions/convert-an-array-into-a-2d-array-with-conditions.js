b2b767b87a71cb8a7a5c4b339c4928101adda4bc
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-02 09:14:09 -0800

    Time: 156 ms (15.46%) | Memory: 45 MB (98.97%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.js
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.js
@@ -3,24 +3,41 @@
  * @return {number[][]}
  */
 var findMatrix = function(nums) {
-    let matrix = [];
-    nums.sort((a, b) => a - b);
+//     let matrix = [];
+//     nums.sort((a, b) => a - b);
 
-    while (nums.length > 0) {
-    let row = [nums.shift()];
+//     while (nums.length > 0) {
+//     let row = [nums.shift()];
 
-    // Add distinct elements to the current row
-    for (let i = 0; i < nums.length; i++) {
-      if (!row.includes(nums[i])) {
-        row.push(nums[i]);
-        nums.splice(i, 1);
-        console.log(nums)
-        i--; // Adjust index after splicing
-      }
+//     // Add distinct elements to the current row
+//     for (let i = 0; i < nums.length; i++) {
+//       if (!row.includes(nums[i])) {
+//         row.push(nums[i]);
+//         nums.splice(i, 1);
+//         console.log(nums)
+//         i--; // Adjust index after splicing
+//       }
+//     }
+
+//     matrix.push(row);
+//   }
+
+//   return matrix;
+
+    let tempA = [];
+
+    for (const num of nums) {
+        let present = false  
+        for (const row of tempA) {
+            if (!row.includes(num)) {
+                row.push(num);
+                present = true;
+                break;
+            }
+        }
+        if(!present){
+        tempA.push([num]);
+        }
     }
-
-    matrix.push(row);
-  }
-
-  return matrix;
+    return tempA;
 };
