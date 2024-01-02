27645e2c22726f3f6e298d5b835da4f47b50e28b
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-02 09:14:24 -0800

    Time: 182 ms (11.34%) | Memory: 45.4 MB (97.94%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.js
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.js
@@ -3,26 +3,7 @@
  * @return {number[][]}
  */
 var findMatrix = function(nums) {
-//     let matrix = [];
-//     nums.sort((a, b) => a - b);
 
-//     while (nums.length > 0) {
-//     let row = [nums.shift()];
-
-//     // Add distinct elements to the current row
-//     for (let i = 0; i < nums.length; i++) {
-//       if (!row.includes(nums[i])) {
-//         row.push(nums[i]);
-//         nums.splice(i, 1);
-//         console.log(nums)
-//         i--; // Adjust index after splicing
-//       }
-//     }
-
-//     matrix.push(row);
-//   }
-
-//   return matrix;
 
     let tempA = [];
 
