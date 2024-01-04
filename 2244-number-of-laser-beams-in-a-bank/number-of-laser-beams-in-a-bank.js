4f32cac7ab1221455bc2f2648448c3fe6c77957f
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-03 23:37:52 -0800

    Time: 67 ms (94.05%) | Memory: 49.2 MB (96.43%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.js
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var numberOfBeams = function(bank) {
-    let curr, prev=ans=0
-  for(let i=0; i<bank.length;i++){
-        curr = 0; // Reset the current count of security devices for each row
-         for (let j = 0; j < bank[0].length; j++) {
-             // Count the number of security devices in the current row
-            if (bank[i][j] == '1') curr++; 
+    let count, prev = ans = 0;
+    for (let i = 0;i<bank.length;i++){
+        count = 0;
+        for (let j = 0;j<bank[0].length;j++){
+            if (bank[i][j]=='1') count++;
         }
-          if (curr != 0) {
-                // Accumulate the product of current count and previous count
-                ans += curr * prev;
-                prev = curr; // Update the previous count for the next iteration
-            }
-  }
-  return ans
+        if (count!==0) {
+            ans += count*prev
+            prev = count;
+        }
+    }
+    return ans;
+
+
 };
