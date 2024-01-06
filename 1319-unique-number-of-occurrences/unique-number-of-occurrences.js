006309783d149ff9e99b9b71a40854cc24f259d2
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-06 14:25:37 -0800

    Time: 55 ms (54.77%) | Memory: 42.8 MB (47.76%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    let map = {}
+    for (let i = 0 ; i < arr.length ; i++){
+        let val = map[arr[i]]
+        if (val == undefined) map[arr[i]] = 1
+        else map[arr[i]] = val + 1
+    }
+    let newMap = {}
+    for (key in map){
+        if (newMap[map[key]]) return false;
+        else newMap[map[key]] = true;
+    }
+    return true;
+};
