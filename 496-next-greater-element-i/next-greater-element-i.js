2cd39153a3d1bb450292a3a0d8493c0bae6217c9
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-09 20:32:13 -0800

    Time: 62 ms (42.27%) | Memory: 42.3 MB (96.13%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -20,40 +20,6 @@ var nextGreaterElement = function(nums1, nums2) {
             }
         }
         blankArr.push(max);
-    // let currElement;
-    // for (let i = 0; i<nums1.length;i++){
-    //     let found = false;
-    //     let hasGreater = false;
-    //     for (let j = 0;j<nums2.length;j++){
-    //         if (nums1[i]==nums2[j]){
-    //             found = true
-    //             for (let k = j+1;k<nums2.length;k++){
-    //                 if (nums2[j+1] > nums1[i]) {
-    //                     blankArr.push(nums2[j+1]);
-    //                     currentElement = nums2[j+1]
-    //                     break;
-    //                 }
-    //                 else if (currentElement<nums2[j+2]){
-    //                     k++;
-    //                 }
-    //                 else {
-    //                     blankArr.push(-1)
-    //                     break;
-    //                 }
-    //             }
-    //             // if (!hasGreater) blankArr.push(-1)
-    //             // break;
-
-    //             // if (nums1[i]<nums2[j+1]) blankArr.push(nums2[j+1])
-    //             // else blankArr.push(-1)
-                
-    //         }
-    //         if(!found) {
-    //             blankArr.push(-1);
-    //             break;
-    //         }
-    // }
-    // }
     }
         return blankArr;
 
