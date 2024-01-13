2a9312f3379284be14c4e3602baf5a607c33122d
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-12 17:15:34 -0800

    Time: 122 ms (72.67%) | Memory: 48.3 MB (34.30%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.js
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -4,9 +4,7 @@
  * @return {number}
  */
 var minSteps = function(s, t) {
-    let maps1 = {}
-    let count = 0;
-    let maps2 = {};
+    let maps1 = {}, count = 0, maps2 = {};
     for(let i = 0;i<s.length;i++){
         let val = maps1[s[i]]
         if (val == undefined) maps1[s[i]] = 1
@@ -18,7 +16,6 @@ var minSteps = function(s, t) {
         else maps2[t[i]] = val + 1
     }
     for (let key in maps2){
-        console.log(maps1[key] , maps2[key])
         count += Math.max(0, maps2[key] - (maps1[key] || 0));
     }
     return count;
