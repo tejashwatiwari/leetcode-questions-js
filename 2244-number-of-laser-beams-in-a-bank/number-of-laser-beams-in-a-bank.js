5080bbdc4bf5867085cf487b51e77c32ff61d3ed
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-19 01:25:11 -0800

    Time: 100 ms (58.37%) | Memory: 50.2 MB (61.88%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.js
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.js
@@ -7,7 +7,7 @@ var numberOfBeams = function(bank) {
     for (let i = 0;i<bank.length;i++){
         let count = 0
         for (let j = 0;j<bank[i].length;j++){
-            if (bank[i][j] == '1') count++
+            if (bank[i][j] == 1) count++
         }
         if (count!==0) {
             ans += count*prev
