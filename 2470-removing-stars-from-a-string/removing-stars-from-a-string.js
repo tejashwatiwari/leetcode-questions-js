e2df8a94ab8fee4f75ff7ec851c5546f18d376eb
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-19 00:38:42 -0800

    Time: 70 ms (97.21%) | Memory: 56.3 MB (86.73%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.js
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.js
@@ -4,9 +4,10 @@
  */
 var removeStars = function(s) {
     let stack = []
-    for (let i = 0 ; i< s.length;i++){
-        if (s[i]== '*') stack.pop()
-        else stack.push(s[i])
+    for (let i = 0;i<s.length;i++){
+        if (s[i]!=='*') stack.push(s[i])
+        else stack.pop()
     }
     return stack.join('')
+    
 };
