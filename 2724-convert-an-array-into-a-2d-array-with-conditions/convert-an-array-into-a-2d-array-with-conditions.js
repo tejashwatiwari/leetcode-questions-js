0b0bb97a476f959fffc3af5f4b9dd18748e80036
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-19 00:34:07 -0800

    Time: 92 ms (80.06%) | Memory: 45.9 MB (50.95%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.js
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.js
@@ -3,41 +3,18 @@
  * @return {number[][]}
  */
 var findMatrix = function(nums) {
-    //     let matrix = [];
-//     nums.sort((a, b) => a - b);
-
-//     while (nums.length > 0) {
-//     let row = [nums.shift()];
-
-//     // Add distinct elements to the current row
-//     for (let i = 0; i < nums.length; i++) {
-//       if (!row.includes(nums[i])) {
-//         row.push(nums[i]);
-//         nums.splice(i, 1);
-//         console.log(nums)
-//         i--; // Adjust index after splicing
-//       }
-//     }
-
-//     matrix.push(row);
-//   }
-
-//   return matrix;
-
-    let tempA = [];
-
-    for (const num of nums) {
-        let present = false  
-        for (const row of tempA) {
-            if (!row.includes(num)) {
-                row.push(num);
-                present = true;
+    let blankArr = []
+    for (let i = 0;i<nums.length;i++){
+        let placed = false;
+        for (let j = 0; j<blankArr.length;j++){
+            if (!blankArr[j].includes(nums[i])){
+                blankArr[j].push(nums[i])
+                placed = true;
                 break;
             }
         }
-        if(!present){
-        tempA.push([num]);
-        }
+        if(!placed) blankArr.push([nums[i]])
+
     }
-    return tempA;
+    return blankArr    
 };
