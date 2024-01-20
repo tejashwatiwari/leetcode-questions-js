55577552adc8b72f8baf8f91dbdd5f89d420c155
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-20 12:48:30 -0800

    Time: 74 ms (62.94%) | Memory: 68.1 MB (8.61%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.js
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.js
@@ -3,7 +3,12 @@
  * @return {boolean}
  */
 var increasingTriplet = function(nums) {
-
+    // let present = false;
+    // for (let i = 0 ; i < nums.length ; i++){
+    //     if (nums[i]<nums[i+1]) present = true;
+    // }
+    // if (!present) return false
+    // return true;
     let first = Number.MAX_SAFE_INTEGER;
     let second = Number.MAX_SAFE_INTEGER;
      for (let num of nums) {
