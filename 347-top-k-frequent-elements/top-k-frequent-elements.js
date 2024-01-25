d3b9f939eea29f3eb29d101845cb3888c641c46c
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-25 14:56:30 -0800

    Time: 382 ms (5.08%) | Memory: 57.6 MB (5.21%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,19 +4,27 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
+    let map={}, frequentArray=[], sortedArray=[], outputArray=[]
+    for(let i=0;i<nums.length;i++){
+        let val=map[nums[i]]
+        if(val==undefined) map[nums[i]]=1
+        else map[nums[i]]=val+1
+    }
 
-    let map = {}, blankArr = [], pairs=[]
-    for (let i = 0;i<nums.length;i++){
-        let val = map[nums[i]]
-        if (val == undefined) map[nums[i]] = 1
-        else map[nums[i]] = val + 1
+    for(key in map) frequentArray.push(map[key])
+
+    frequentArray.sort((a,b)=>b-a)
+    for(let i=0;i<frequentArray.length;i++){
+        for(key in map){
+            let val=map[key]
+            if(val==frequentArray[i]){
+                if(k>0){
+                    outputArray.push(key)
+                    map[key]=null
+                    k--
+                }
+            }
+        }
     }
-    for (keys in map){
-        blankArr.push([keys, map[keys]])
-    }
-    blankArr.sort((a,b)=>(b[1]-a[1]))
-    for(let i = 0;i<k;i++){
-        pairs.push(blankArr[i][0])
-    }
-    return pairs
+return outputArray
 };
