d906f9ec3784efa9cf6013e6876ff4397db69056
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-25 11:35:52 -0800

    Time: 64 ms (81.24%) | Memory: 50.3 MB (9.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,24 +4,18 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    //check if the length is equal
-    if (s.length!==t.length) return false;
-    
-        let map1={};
-        for (let i =0;i<s.length;i++){
-            let value = map1[s[i]];
-            if (value == undefined){
-                map1[s[i]] = 1;
-            }else{
-                map1[s[i]] = value + 1;
-            }
-        }
-        
-        for(let i=0; i<t.length;i++){
-            let value = map1[t[i]];
+    if (s.length!==t.length) return false
+    let maps1 = {}, maps2={}
+    for (let i = 0;i<s.length;i++){
+        let val = maps1[s[i]]
+        if (val == undefined) maps1[s[i]] = 1
+        else maps1[s[i]] = val + 1
+    }
+      for(let i=0; i<t.length;i++){
+            let value = maps1[t[i]];
             if(value==undefined || value==0) return false
-            map1[t[i]]=value-1   
-        }
-return true
+            maps1[t[i]]=value-1   
+    }
+    return true
     
 };
