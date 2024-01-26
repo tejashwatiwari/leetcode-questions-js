778bff5fe8045885fdd485983721550ead9b9365
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-25 16:14:17 -0800

    Time: 60 ms (42.80%) | Memory: 50.2 MB (5.00%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -4,12 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let l = 0;
-    let r = numbers.length-1;
-    while(numbers[l] + numbers[r] !=target ){
-        if (numbers[l]+numbers[r]>target) r--;
+    let l = 0, r= numbers.length-1
+    while (numbers[l]+numbers[r]!=target){
+        if(numbers[l]+numbers[r]>target) r--;
         else l++;
     }
+    // for (let i = 0;i<numbers.length;i++){
+    //     let diff = target - numbers[l]
+    //     if(diff!==numbers[r]) r--
+    //     else l++
+    // }
     return [l+1,r+1];
-    
 };
