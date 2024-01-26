e94d2b762e6561cacc3087193a4f5c8de6d44f6f
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-26 11:17:10 -0800

    Time: 54 ms (62.96%) | Memory: 49.4 MB (5.56%) - LeetSync

---
+++ 2401-count-asterisks/count-asterisks.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countAsterisks = function(s) {
+    let count = 0;
+    let line = 0
+    for (let i = 0;i<s.length;i++){
+        if (s[i]=='|') line++
+        if(line%2==0){
+            if (s[i]=='*') count++;
+        }
+    }
+    return count;
+};
