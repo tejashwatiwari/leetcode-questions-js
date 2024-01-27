1ddbded12d57f544faa25332c1b4d6eb9b8183a2
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-26 20:44:38 -0800

    Time: 50 ms (86.93%) | Memory: 49.9 MB (10.89%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,14 +3,15 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    if(s.length==1)return false
-    let stack = []
-    for (let i =0;i<s.length;i++){
-        let char=s[i], top=stack[stack.length-1]
-        if (char=='(' || char=='[' || char=='{')stack.push(char);
-        else if((char==')' && top=='(') ||(char==']' && top=='[')|| (char=='}' && top=='{'))stack.pop()
-        else stack.push(char)    
-    }
-    if (stack.length == 0) return true;
+    if (s.length == 1) return false;
+    let stack = [];
+    for (let i =0 ; i<s.length;i++){
+        let top = stack[stack.length-1]
+        if (s[i]=='(' || s[i]=='[' || s[i]=='{') stack.push(s[i])
+        else if (s[i]==')' && top=='(' || s[i]==']' && top=='[' || s[i]=='}' && top=='{') stack.pop()   
+        else stack.push(s[i])
+        }
+    if (stack.length==0) return true
     else return false
+    
 };
