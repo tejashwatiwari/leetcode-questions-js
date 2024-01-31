7be785d578ff320d06db2d8c56b539977447aa92
Tejashwa Tiwari <tejashwatiwari@gmail.com>
2024-01-30 16:03:00 -0800

    Time: 60 ms (98.53%) | Memory: 60 MB (45.59%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.js
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.js
@@ -6,8 +6,9 @@ var minSwaps = function(s) {
     let count = 0, vari = 0
     for (let i = 0;i<s.length;i++){
         if (s[i]=='[') count++
-        else if (count == 0) {vari++
-        count++
+        else if (count == 0) {
+            vari++
+            count++
         }
         else count--
     } 
