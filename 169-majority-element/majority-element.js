ee82cc69b8ba7bf3a25c63ad747a9df256864051
Tejashwa <tejashwatiwari@gmail.com>
2024-02-11 19:14:00 -0800

    Time: 66 ms (32.07%) | Memory: 50.6 MB (31.20%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let maps = {};
+    let max = 0;
+    for (let i =0;i<nums.length;i++){
+        let values = maps[nums[i]]
+        if (values == undefined){
+            maps[nums[i]]=1
+        }else{
+            maps[nums[i]] = values +1
+        }
+    }
+    for (key in maps){
+    if (maps[key]>=(nums.length/2)){
+    return key
+    }
+   
+  }
+    
+      
+};
