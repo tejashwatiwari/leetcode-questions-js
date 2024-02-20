acfd33d7bf61f2993766566ee4d16fe71237120c
Tejashwa <tejashwatiwari@gmail.com>
2024-02-19 17:31:55 -0800

    Time: 267 ms (10.72%) | Memory: 50.7 MB (47.92%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,8 +3,7 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    nums.sort((a,b)=>a-b) 
-    for (let i = 0;i<=nums.length;i++){
-        if(i!==nums[i]) return i
-    }
+    for(let i=0; i<=nums.length;i++){
+       if(nums.indexOf(i)==-1)return i
+   }
 };
