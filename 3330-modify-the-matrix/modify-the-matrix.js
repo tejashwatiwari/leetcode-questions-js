59f9b685185ed7b69cf93d0e1e732b4c8c0aa858
Tejashwa <tejashwatiwari@gmail.com>
2024-03-01 15:35:50 -0800

    Time: 76 ms (82.41%) | Memory: 52.5 MB (96.52%) - LeetSync

--- 3330-modify-the-matrix/modify-the-matrix.js
+++ 3330-modify-the-matrix/modify-the-matrix.js
@@ -6,16 +6,8 @@ var modifiedMatrix = function(matrix) {
     let column = matrix.length,row = matrix[0].length;
     for (let i = 0; i < row; i++) { 
         let maxColumn = 0;
-        for (let j = 0; j < column; j++) {
-            maxColumn = Math.max(maxColumn, matrix[j][i]);
-        }
-        for (let k = 0; k < column; k++) { 
-            if (matrix[k][i] === -1) matrix[k][i] = maxColumn; 
-        }
+        for (let j = 0; j < column; j++) maxColumn = Math.max(maxColumn, matrix[j][i]);
+        for (let k = 0; k < column; k++) if (matrix[k][i] === -1) matrix[k][i] = maxColumn; 
     }
-
     return matrix;
-
-    
-    
 };
