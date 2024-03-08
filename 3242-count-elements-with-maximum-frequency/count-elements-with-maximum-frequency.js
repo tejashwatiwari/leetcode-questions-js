030dd92bfceb98fb4bfa4256953297962f92dee7
Tejashwa <tejashwatiwari@gmail.com>
2024-03-08 12:05:05 -0800

    Time: 51 ms (94.73%) | Memory: 51.4 MB (33.68%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.js
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.js
@@ -3,28 +3,21 @@
  * @return {number}
  */
 var maxFrequencyElements = function(nums) {
-        let maps = {};
+    let maps = {};
     for (let i =0;i<nums.length;i++){
     let val = maps[nums[i]];
-    if (val == undefined){
-      maps[nums[i]]=1;
-    }else {
-      maps[nums[i]] = val +1;
+    if (val == undefined) maps[nums[i]]=1;
+    else maps[nums[i]] = val +1;
     }
-  }
     let max = 0;
-
     for (keys in maps){
-    
         if (maps[keys]>max) max = maps[keys];
-    }
-    let total_max = 0;
+        }
+        let total_max = 0;
     for (let key in maps) {
         if (maps[key] == max) {
             total_max += max;
         }
     }
-
     return total_max;
-    
 };
