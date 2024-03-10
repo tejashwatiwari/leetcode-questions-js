76bd005907742f089e7982fa016e6e266cf69943
Tejashwa <tejashwatiwari@gmail.com>
2024-03-09 17:26:41 -0800

    Time: 90 ms (5.39%) | Memory: 55.8 MB (5.39%) - LeetSync

---
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} number
+ * @param {character} digit
+ * @return {string}
+ */
+var removeDigit = function(number, digit) {
+    let max = ''
+    for (let i = 0; i < number.length; i++) {
+        if (number[i] === digit) {
+            let temp = number.slice(0, i) + number.slice(i + 1)
+            console.log(i,i+1)
+            console.log(temp,"temp")
+            if (temp > max) max = temp
+        }
+    }
+    return max
+}
