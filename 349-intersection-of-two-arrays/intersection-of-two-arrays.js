29303f2196f6546489c19f2b8382b59cd3828d65
Tejashwa <tejashwatiwari@gmail.com>
2024-03-09 16:21:55 -0800

    Time: 55 ms (60.14%) | Memory: 51.4 MB (7.75%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -6,20 +6,16 @@
 var intersection = function(nums1, nums2) {
      const map = {}; // Hashmap to store counts of elements
     let blankArr = []
-
     // Populate hashmap with counts of elements from nums1
     for (const num of nums1) {
         map[num] = (map[num] || 0) + 1;
     }
-    
     for (let j=0;j<nums2.length;j++){
         let val = map[nums2[j]]
         if (val!==undefined){
-            // map[nums2[j]]=val-1            
             if(!blankArr.includes(nums2[j])) 
             blankArr.push(nums2[j])       
         }
     }
    return blankArr 
-   
-}
+};
