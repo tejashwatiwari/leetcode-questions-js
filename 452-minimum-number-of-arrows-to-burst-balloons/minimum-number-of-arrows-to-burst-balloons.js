ecbdba7623c5390f219a87e6a663c26ac13dbd88
Tejashwa <tejashwatiwari@gmail.com>
2024-03-18 11:38:01 -0700

    Time: 178 ms (99.12%) | Memory: 72.1 MB (92.94%) - LeetSync

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+        points.sort((a,b)=>a[0]-b[0])
+    let arrows=1, end=points[0][1]
+
+    for(let i=1; i<points.length;i++){
+        if(points[i][0]>end){
+            arrows++
+            end=points[i][1]
+        }else end=Math.min(end, points[i][1])
+    }
+return arrows
+};
