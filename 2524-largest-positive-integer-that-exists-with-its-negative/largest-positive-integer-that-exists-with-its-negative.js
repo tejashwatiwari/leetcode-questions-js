5074fba661cd6bbb843e7ed0870cdc4ffeebb0a8
Tejashwa <tejashwatiwari@gmail.com>
2024-05-01 22:24:23 -0700

    Time: 74 ms (45.22%) | Memory: 51.3 MB (70.00%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.js
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.js
@@ -4,13 +4,9 @@
  */
 var findMaxK = function(nums) {
     nums.sort((a,b)=>(b-a))
-    let max = 0, max_nega = 0
     for (let i = 0;i<nums.length;i++){
-        let nega = nums[i]*(-1)
-        if(nums.includes(nega)) return nums[i]
-        else {
-            nega = 0
-        };
+        // let nega = nums[i]*(-1)
+        if(nums.includes(-nums[i])) return nums[i]
     }
     return -1
     
