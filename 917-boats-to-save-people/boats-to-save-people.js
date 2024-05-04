13597fced04b07f68e9056c4e81bf06fc07dae95
Tejashwa <tejashwatiwari@gmail.com>
2024-05-03 17:40:11 -0700

    Time: 489 ms (5.51%) | Memory: 63 MB (5.51%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.js
+++ 917-boats-to-save-people/boats-to-save-people.js
@@ -5,14 +5,14 @@
  */
 var numRescueBoats = function(people, limit) {
     people.sort((a,b)=>(a-b))
-    let res = 0
+    let count = 0
     let l = 0, r = people.length-1
     while(l<=r){
         let diff = limit - people[r]
         console.log(diff)
-        res++
+        count++
         r--
         if(diff >= people[l]) l++;
     }
-    return res;
+    return count;
 };
