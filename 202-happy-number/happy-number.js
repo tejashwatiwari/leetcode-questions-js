47ba1f6f4c094eb4b28d5c9096884b579fb71412
Tejashwa <tejashwatiwari@gmail.com>
2024-05-17 16:23:19 -0700

    Time: 48 ms (93.25%) | Memory: 49 MB (95.76%) - LeetSync

---
+++ 202-happy-number/happy-number.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    let strNum = n.toString();
+    let total = 0;
+
+    for(let j = 0;j<7;j++){
+        for(let i = 0;i<strNum.length;i++){
+            total += parseInt(strNum[i])**2;
+        }
+
+        if(total == 1){
+            return true;
+        }
+    
+        strNum = total.toString();
+        total = 0;
+    }
+
+    return false;
+};
