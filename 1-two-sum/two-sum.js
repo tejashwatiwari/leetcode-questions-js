01d8230c20cf53c7cffc8840fde5b5f1fde15852
Tejashwa <tejashwatiwari@gmail.com>
2024-06-12 11:43:28 -0700

    Time: 52 ms (90.42%) | Memory: 50.9 MB (42.74%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,10 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let maps = {}
+   let maps = {}
     for (let i = 0;i<nums.length;i++){
         let finalNum = target - nums[i]
-        if (maps[finalNum]!==undefined) {return [maps[finalNum],i]}
+        if (maps[finalNum]!= undefined) {return [maps[finalNum],i]}
          maps[nums[i]] = i;
     }
 };
