cbe5efd36a850bc03ff04a2b6ee303b4c32e72d3
Tejashwa <tejashwatiwari@gmail.com>
2024-06-15 11:08:39 -0700

    Time: 287 ms (56.91%) | Memory: 84.8 MB (42.28%) - LeetSync

---
+++ 502-ipo/ipo.js
@@ -0,0 +1,98 @@
+/**
+ * @param {number} k
+ * @param {number} w
+ * @param {number[]} profits
+ * @param {number[]} capital
+ * @return {number}
+ */
+var findMaximizedCapital = function(k, w, profits, capital) {
+    const projects = capital.map((c, i) => [c, profits[i]]);
+    projects.sort((a, b) => a[0] - b[0]);
+
+    const maxHeap = new MaxHeap();
+    let i = 0;
+
+    for (let j = 0; j < k; j++) {
+        while (i < projects.length && projects[i][0] <= w) {
+            maxHeap.insert(projects[i][1]);
+            i++;
+        }
+
+        if (maxHeap.isEmpty()) break;
+        w += maxHeap.extractMax();
+    }
+
+    return w;
+};
+
+class MaxHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(val) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+
+    extractMax() {
+        if (this.heap.length === 1) {
+            return this.heap.pop();
+        }
+
+        const max = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return max;
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[parentIndex] >= this.heap[index]) break;
+
+            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+            index = parentIndex;
+        }
+    }
+
+    bubbleDown() {
+        let index = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.heap[leftChildIndex];
+                if (leftChild > element) {
+                    swap = leftChildIndex;
+                }
+            }
+
+            if (rightChildIndex < length) {
+                rightChild = this.heap[rightChildIndex];
+                if (
+                    (swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)
+                ) {
+                    swap = rightChildIndex;
+                }
+            }
+
+            if (swap === null) break;
+            this.heap[index] = this.heap[swap];
+            this.heap[swap] = element;
+            index = swap;
+        }
+    }
+
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+}
