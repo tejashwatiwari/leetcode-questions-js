e52c0c11f2e6caa9d8e06b601f6df3acda7b29e8
Tejashwa <tejashwatiwari@gmail.com>
2024-06-28 18:21:26 -0700

    Time: 1795 ms (5.66%) | Memory: 86.3 MB (18.87%) - LeetSync

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.js
@@ -0,0 +1,14 @@
+var getAncestors = function (n, edges) {
+    let aps = Array(n).fill().map(() => new Set())
+    for (let [x, y] of edges) {
+        aps[y].add(x)
+    }
+    for (let ps of aps) {
+        for (let p of ps) {
+            for (let gp of aps[p]) {
+                ps.add(gp)
+            }
+        }
+    }
+    return aps.map(ps => [...ps].sort((a, b) => a - b))
+};
