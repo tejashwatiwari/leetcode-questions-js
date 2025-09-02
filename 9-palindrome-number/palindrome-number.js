e3a456f5f61277468152403b15b0b6dc108298c4
Tejashwa <tejashwatiwari@gmail.com>
2025-09-02 13:13:59 -0700

    Time: 14 ms (15.08%) | Memory: 63.4 MB (92.48%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x<0) return false
+    let rev = 0
+    let xCopy = x
+    while (x > 0){
+         let rem = x % 10
+         rev = (10*rev) + rem
+         x = Math.floor(x/10)
+    }
+    return rev == xCopy
+};
