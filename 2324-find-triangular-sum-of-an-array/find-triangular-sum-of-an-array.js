b2e35efb4d43320847adf158bbe4da1b0e19ddb5
Tejashwa <tejashwatiwari@gmail.com>
2025-09-30 11:09:39 -0700

    Time: 101 ms (51.67%) | Memory: 60.4 MB (41.67%) - LeetSync

---
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangularSum = function(nums) {
+       while(nums.length > 1){
+
+    let newNums = [];
+    for(let i=0; i< nums.length - 1; i++){
+        newNums.push( (nums[i] + nums[i+1]) % 10 );
+    }
+
+    nums = newNums;
+
+   }
+
+   return nums[0];
+};
