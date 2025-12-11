27a575ea74b6125024a97c6963941c27c7e8f7f2
Tejashwa <tejashwatiwari@gmail.com>
2025-12-11 11:14:51 -0800

    Time: 248 ms (44.13%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,12 @@
+SELECT 
+    s.user_id,
+    ROUND(AVG(
+        CASE
+            WHEN action = 'confirmed' THEN 1
+            ELSE 0
+        END
+     )::numeric , 2) AS confirmation_rate
+FROM signups s  
+LEFT JOIN confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id
