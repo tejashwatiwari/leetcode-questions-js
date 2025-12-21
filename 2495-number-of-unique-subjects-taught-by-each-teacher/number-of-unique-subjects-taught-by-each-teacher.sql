04fdbc015f0dfb316e68c5227de58088b2bfa5b2
Tejashwa <tejashwatiwari@gmail.com>
2025-12-21 09:13:08 -0800

    Time: 617 ms (18.89%) | Memory: 0B (100.00%) - LeetSync

--- 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-select  teacher_id, count(DISTINCT subject_id) as cnt from Teacher group by teacher_id
+select  teacher_id, COALESCE(count(DISTINCT subject_id),0) as cnt from Teacher group by teacher_id
