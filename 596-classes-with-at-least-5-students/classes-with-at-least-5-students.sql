5ee509bc114c2736b48ae4cf7306aa3b52519251
Tejashwa <tejashwatiwari@gmail.com>
2025-12-21 09:23:59 -0800

    Time: 285 ms (52.55%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,3 +1,3 @@
 -- Write your PostgreSQL query statement below
-with tc as (select class, count(distinct student) as ds from Courses GROUP BY class)
+with tc as (select class, count(student) as ds from Courses GROUP BY class)
 select class from tc where ds >= 5
