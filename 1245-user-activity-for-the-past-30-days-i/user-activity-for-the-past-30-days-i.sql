6b205643dcab8d32caa8727be5f068135dd8c00c
Tejashwa <tejashwatiwari@gmail.com>
2025-12-21 09:35:07 -0800

    Time: 382 ms (32.42%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,2 @@
+-- Write your PostgreSQL query statement below
+select activity_date as day, count(distinct user_id) as active_users from activity  where activity_date between date '2019-07-28' - interval '30 days' and date '2019-07-27' group by activity_date
